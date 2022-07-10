c18c0d67a5d95e130d4e08d39ed54da9afb84098
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2022-07-10 13:28:36 -0400

    Partial functions wooo

--- src/algo_pg/stat_calculators.py
+++ src/algo_pg/stat_calculators.py
@@ -23,22 +23,23 @@ def dummy_420_69(df, last_row_index):
     return -420.69
 
 
-def avg_last_5(df, last_row_index):
+def avg_last_n(df, last_row_index, window_size=5):
     """
-    Computes the average volume-weighted average price over the last 5 TimeFrames.
+    Computes the average volume-weighted average price over the last n TimeFrames.
 
     Args:
         df: A dataframe of raw input data, almost straight from Alpaca.
         last_row_index: The index of the last valid row in the provided dataframe.
+        window_size: The number of TimeFrames to go back for the average.
 
     Returns:
-        The average volume-weighted average price over the last 5 TimeFrames.
+        The average volume-weighted average price over the last n TimeFrames.
 
     """
 
     total = 0
 
-    for _, row in df.loc[last_row_index - 4:last_row_index].iterrows():
+    for _, row in df.loc[last_row_index - window_size - 1:last_row_index].iterrows():
         total += row.vwap
 
     avg = total / 5
@@ -46,19 +47,20 @@ def avg_last_5(df, last_row_index):
     return avg
 
 
-def net_last_5(df, last_row_index):
+def net_last_n(df, last_row_index, window_size=5):
     """
-    Computes the net value over the last 5 TimeFrames.
+    Computes the net value over the last n TimeFrames.
 
     Args:
         df: A dataframe of raw input data, almost straight from Alpaca.
         last_row_index: The index of the last valid row in the provided dataframe.
+        window_size: The number of TimeFrames to go back for the average.
 
     Returns:
-        The net value over the last 5 TimeFrames.
+        The net value over the last n TimeFrames.
 
     """
 
-    net = df.loc[last_row_index].vwap - df.loc[last_row_index - 4].vwap
+    net = df.loc[last_row_index].vwap - df.loc[last_row_index - window_size - 1].vwap
 
     return net
--- tests/scratchpads/jakes_scratchpad.py
+++ tests/scratchpads/jakes_scratchpad.py
@@ -2,10 +2,11 @@ from algo_pg.algorithms.bang_bang.alg import BangBang
 from algo_pg.signals import AlpacaDataManager
 from algo_pg.machine import TradingMachine, DataSettings
 from algo_pg.portfolio import Portfolio
-from algo_pg.stat_calculators import avg_last_5, net_last_5
+from algo_pg.stat_calculators import avg_last_n, net_last_n
 from algo_pg.util import apg_init
 from alpaca_trade_api import TimeFrame
 from datetime import timedelta
+from functools import partial
 
 
 def main():
@@ -14,8 +15,10 @@ def main():
     # Keys will become column names and the function object that is the value will be
     # called on every row of every Position's DataManager
     stat_dict = {
-        "avg_l5_vwap": avg_last_5,
-        "net_l5_vwap": net_last_5
+        "avg_l5_vwap": partial(avg_last_n, window_size=5),
+        "net_l5_vwap": partial(net_last_n, window_size=5),
+        "avg_l10_vwap": partial(avg_last_n, window_size=10),
+        "net_l10_vwap": partial(net_last_n, window_size=10),
     }
 
     # A dataclass that stores general information about data settings and how the data
